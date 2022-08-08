c4cd4997777cb1606cb365bb26924073d2b5dbe3
tiago369 <tiagobarreto581@gmail.com>
2022-08-08 17:07:37 -0300

    first commit

---
+++ codes/3-4-program.cpp
@@ -0,0 +1,14 @@
+#include "../include/std_lib_facilities.h"
+
+int main()
+{
+    int contar;
+    cin >> contar;
+    string nome;
+    cin >> nome;
+
+    int c2 = contar + 2;
+    string s2 = nome + "Jr.";
+
+    int 
+}
